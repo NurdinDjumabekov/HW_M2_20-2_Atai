53a762c20c15fbfc097368ec97ce6a4abb27691a
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-11-18 13:27:53 +0600

    Add files via upload

---
+++ DZ2_Nurdin_20-01/main.js
@@ -0,0 +1,19 @@
+for (let i = 0; i <= 50; i++) {
+  console.log(i);
+}
+let j = 0;
+while (j < 50) {
+  j++;
+  console.log(j);
+}
+//////////////////////////////////////////////////////////////////////
+const arr = ["Привет", "мир", "!"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+console.log(arr);
+let a = arr[0];
+let b = arr[1];
+let c = arr[2];
+console.log(a, b, c);
+
