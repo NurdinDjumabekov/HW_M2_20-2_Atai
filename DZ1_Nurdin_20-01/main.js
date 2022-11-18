973a10baab7ba24abbad533cc9b7dce826c13939
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-11-18 12:57:34 +0600

    Add files via upload

---
+++ DZ1_Nurdin_20-01/main.js
@@ -0,0 +1,19 @@
+let a = Number(prompt("Выберите : 1.цикл от 1 - 50 или 2. Цикл от 50 - 100"));
+if (a === 1) {
+  for (let i = 0; i <= 50; i++) {
+    console.log(i);
+  }
+} else if (a === 2) {
+  for (let j = 50; j <= 100; j++) {
+    console.log(j);
+  }
+} else {
+  console.log(error);
+}
+const b = 10;
+let c = null;
+let d = true;
+let e = "hello";
+let f = false;
+let g = undefined;
+let h = "###%@";
