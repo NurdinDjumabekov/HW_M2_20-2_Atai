c66770d57ad371d6c1722e5a5a6330fe524b6b20
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-11-19 22:30:57 +0600

    Add files via upload

---
+++ DZ3_Nurdin_20-01/main.js
@@ -0,0 +1,39 @@
+///////////////////////////1111111111111////////////////////////////////////
+let a = prompt("Введите число : ");
+if (a > 0) {
+  let ggg = function functionFun() {
+    return console.log("!");
+  };
+  ggg()
+//   console.log(ggg);
+} else {
+  let ggg = function functionFun() {
+    return console.log("!!");
+  };
+  ggg()
+//   console.log(ggg);
+}
+/////////////////////////////222222222222222222////////////////////////////////////
+// let arr = [];
+// for (let i = 0; i < 10; i++) {
+//   arr.push("x");
+// }
+// console.log(arr);
+// let arr = [];
+// for (let i = 0; i < 16; i++) {
+//   arr[i] = "x";
+// }
+// console.log(arr);
+/////////////////////////////333333333333333333////////////////////////////////////
+// function funcOne() {
+//   var i = 1;
+//   return function () {
+//     return i++;
+//   };
+// }
+// var FUNC = funcOne();
+// console.log(FUNC());
+// console.log(FUNC());
+// console.log(FUNC());
+// console.log(FUNC());
+// console.log(FUNC());
